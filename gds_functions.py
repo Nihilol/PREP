ef7b70c30d948fcb87def6fa57a2910ff3ffe958
Oliver Liebe <Oliverliebe@hotmail.com>
2023-03-20 21:43:55 +0100

    Yeh

--- gds_functions.py
+++ gds_functions.py
@@ -3,11 +3,17 @@ import gdspy
 from matplotlib import pyplot as plt
 
 
-gdsii = gdspy.GdsLibrary(infile="1x4_inner_structure.gds")
-main_cell = gdsii.top_level()[0]  # Assume a single top level cell
-print(main_cell)
-#points = main_cell.polygons[0].polygons[0]
-#for p in points:
-#    print("Points: {}".format(p)) 
-
-gdspy.LayoutViewer(gdsii)
+def gds_cut(read_file, x_lims, y_lims, out_file, view = False):
+    loaded_lib = gdspy.GdsLibrary(infile = read_file)
+    new_lib = gdspy.GdsLibrary()
+    main_cell = loaded_lib.top_level()[0] 
+    cell1 = new_lib.new_cell('Sliced')
+    for i in main_cell.polygons:
+        i1 = gdspy.slice(i, x_lims, axis = 0, layer = i.layers[0])
+        i2 = gdspy.slice(i1[1], y_lims, axis = 1, layer = i.layers[0])
+        cell1.add(i2[1])
+    
+    new_lib.write_gds(name = out_file)
+    if view:
+        new_gds = gdspy.GdsLibrary(infile = out_file)
+        gdspy.LayoutViewer(new_gds)
